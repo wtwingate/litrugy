420a30d05d1c1f67a2324b0cdad0ec222c42e641
William Wingate <wtwingate@gmail.com>
2024-06-25 22:42:37 -0400

    Begin writing models for lectionary app

--- website/lectionary/models.py
+++ website/lectionary/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Years(models.IntegerChoices):
+    A = 0
+    B = 1
+    C = 2
+
+
+class Ranks(models.IntegerChoices):
+    pass
+
+
+class Colors(models.TextChoices):
+    WHITE = ("WH", "White")
+    RED = ("RD", "Red")
+    GREEN = ("GR", "Green")
+    VIOLET = ("VI", "Violet")
+    ROSE = ("RO", "Rose")
+    BLACK = ("BK", "Black")
+    BLUE = ("BL", "Blue")
+
+
+class Day(models.Model):
+    name = models.CharField(max_length=200)
+    alt_name = models.CharField(max_length=200, null=True, blank=True)
+    year = models.IntegerField(choices=Years)
+    season = models.ForeignKey("Season", on_delete=models.PROTECT)
+    rank = models.IntegerField(choices=Ranks)
+    color = models.Choices(choices=Colors, default=Colors.WHITE)
+    lessons = models.ManyToManyField("Lesson")
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    # A lesson may provide alternate readings
+    scripture = models.CharField(max_length=100)
+    alt_scripture = models.CharField(max_length=100, null=True, blank=True)
+    alt_alt_scripture = models.CharField(max_length=100, null=True, blank=True)
+    days = models.ManyToManyField("Day")
