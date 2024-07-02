58e2ac03e41e3f783474d2833e54ce741262aa61
William Wingate <wtwingate@gmail.com>
2024-07-01 21:04:28 -0400

    Set up Admin pages for Day and Lesson objects

--- lectionary/admin.py
+++ lectionary/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from lectionary.models import Day, Lesson
+
+
+class PropersInline(admin.TabularInline):
+    model = Day.lessons.through
+    extra = 0
+
+
+class DayAdmin(admin.ModelAdmin):
+    inlines = [
+        PropersInline,
+    ]
+    exclude = ["lessons"]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [
+        PropersInline,
+    ]
+    exclude = ["days"]
+
+
+admin.site.register(Day, DayAdmin)
+admin.site.register(Lesson, LessonAdmin)
